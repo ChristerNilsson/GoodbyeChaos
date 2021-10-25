1c9ec579eae136414d061b5a24b5f22a48604f86
ChristerNilsson <janchrister.nilsson@gmail.com>
2021-10-25 20:59:03 +0200

    original

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+
+<script>
+	// https://www.youtube.com/watch?v=Pp40Ejkk2Wc&t=33s&ab_channel=edynurcahyo
+	import { GoogleAuth } from '@beyonk/svelte-social-auth'
+	let data
+</script>
+
+<GoogleAuth clientId="810501500016-fod41p7083pb94hkjsc8ls2tq21ftssd.apps.googleusercontent.com" on:auth-success={e => data = e.detail.user} />
+
+<h1>{#if data}Welcome {data.nt.Yt} !{:else}Not logged in.{/if}</h1>
